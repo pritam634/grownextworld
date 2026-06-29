1b011ccacbad66f7779efb3f59f31804fa0f71e7
Pritam Panda <pritampanda634@gmail.com>
2026-06-29 21:09:09 +0530

    Updated All

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,42 @@
+
 // window scroll > 100
-$(window).scroll(function() {
+$(window).scroll(function () {
         if ($(this).scrollTop() > 200) {
                 $('#arrowBtn').removeClass('hidden');
         } else {
                 $('#arrowBtn').addClass('hidden');
         }
+});
+
+// scroll to top
+function scrollToTop() {
+        window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+        });
+}
+
+
+$(".faq-btn").click(function () {
+
+        let content = $(this).next(".faq-content");
+
+        if (content.is(":visible")) {
+
+                content.slideUp(300);
+
+                $(this).find(".faq-icon").text("+");
+
+        } else {
+
+                $(".faq-content").slideUp(300);
+
+                $(".faq-icon").text("+");
+
+                content.slideDown(300);
+
+                $(this).find(".faq-icon").text("−");
+
+        }
+
 });
