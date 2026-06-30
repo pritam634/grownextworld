febde5fbed47cf4f0544d8c322b32e80373b6b6c
Pritam Panda <pritampanda634@gmail.com>
2026-07-01 01:02:58 +0530

    Updated All files

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,180 @@ function scrollToTop() {
         });
 }
 
+// all swiper
+var swiper = new Swiper(".testimonialSwiper", {
 
+        slidesPerView: 1,
+
+        spaceBetween: 30,
+
+        loop: true,
+
+        speed: 900,
+
+        autoplay: {
+
+                delay: 5000,
+
+                disableOnInteraction: false,
+
+        },
+
+        pagination: {
+
+                el: ".swiper-pagination",
+
+                clickable: true,
+
+        },
+
+        navigation: {
+
+                nextEl: ".testimonial-next",
+
+                prevEl: ".testimonial-prev",
+
+        },
+
+});
+var swiper = new Swiper(".clientSwiper", {
+
+    slidesPerView: 1,
+
+    spaceBetween: 30,
+
+    loop: true,
+
+    autoplay: {
+
+        delay: 4500,
+
+        disableOnInteraction: false,
+
+    },
+
+    pagination: {
+
+        el: ".swiper-pagination",
+
+        clickable: true,
+
+    },
+
+    navigation: {
+
+        nextEl: ".client-next",
+
+        prevEl: ".client-prev",
+
+    },
+
+    breakpoints: {
+
+        768: {
+            slidesPerView: 2,
+        },
+
+        1200: {
+            slidesPerView: 3,
+        }
+
+    }
+
+});
+
+
+new Swiper(".courseSwiper", {
+
+        loop: true,
+
+        spaceBetween: 30,
+
+        autoplay: {
+                delay: 4000,
+        },
+
+        breakpoints: {
+
+                320: {
+                        slidesPerView: 1,
+                },
+
+                768: {
+                        slidesPerView: 2,
+                },
+
+                1200: {
+                        slidesPerView: 3,
+                }
+
+        }
+
+});
+
+new Swiper(".founderSwiper", {
+
+        loop: true,
+
+        speed: 900,
+
+        autoplay: {
+
+                delay: 5000,
+
+                disableOnInteraction: false,
+
+        },
+
+        navigation: {
+
+                nextEl: ".founder-next",
+
+                prevEl: ".founder-prev",
+
+        }
+
+});
+
+new Swiper(".certificateSwiper", {
+
+        slidesPerView: 1,
+
+        spaceBetween: 30,
+
+        loop: true,
+
+        autoplay: {
+                delay: 3500,
+        },
+
+        pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+        },
+
+        breakpoints: {
+
+                640: {
+                        slidesPerView: 2,
+                },
+
+                1024: {
+                        slidesPerView: 3,
+                },
+
+                1280: {
+                        slidesPerView: 4,
+                }
+
+        }
+
+});
+
+
+
+
+// faq 
 $(".faq-btn").click(function () {
 
         let content = $(this).next(".faq-content");
